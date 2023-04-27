16d43e45966828c137714300d475ebabf8cff674
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-04-27 23:24:53 +0530

    added firebase credentials

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+	production:false,
+	firebase:{
+		apiKey: 'AIzaSyA0dZfLCQSJqb82WSbFeG_yZKh7NGg138c',
+		authDomain: 'angularplayground-brp.firebaseapp.com',
+		projectId: 'angularplayground-brp',
+		storageBucket: 'angularplayground-brp.appspot.com',
+		messagingSenderId: '806610591836',
+		appId: '1:806610591836:web:3f74211f5e01788c7b0699',
+		measurementId: 'G-300WXYER8G'
+	}
+}
